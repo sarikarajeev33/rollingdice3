25320e5b29068b325742af06c9560e41b1fea619
sarikarajeev33 <51034115+sarikarajeev33@users.noreply.github.com>
2021-01-20 21:56:26 -0500

    Add files via upload

---
+++ count_six_seven.c
@@ -0,0 +1,23 @@
+/*
+Write a header comment here about the authors and purpose of this file.
+*/
+
+//This all all the include files I can image you needing.
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void){
+    
+    //This time seeds a random number generator. 
+    srand(time(0));
+    
+    //This time generates a random number.
+    rand();
+    
+    //this line generates a num random number.
+    rand();
+ 
+    //Returning 0 inidicates success to the OS
+    return 0;
+}
